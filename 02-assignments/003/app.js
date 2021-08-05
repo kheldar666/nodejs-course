b02430744b0440b90efaa073baa49008e57bd4ad
Martin Papy <martin.papy@gmail.com>
2021-08-05 19:09:47 +0700

    3rd Assignment

--- 02-assignments/003/app.js
+++ 02-assignments/003/app.js
@@ -1,7 +1,17 @@
+const path = require('path');
+
 const express = require('express')
 
 const app = express();
 
+const adminRoute = require('./routes/admin');
+const usersRoute = require('./routes/users');
+const rootDir = require('./utils/path');
 
+app.use(express.static(path.join(rootDir,'public')));
+
+app.use('/admin/',adminRoute);
+
+app.use(usersRoute);
 
 app.listen(3000)
--- 02-assignments/003/routes/users.js
+++ 02-assignments/003/routes/users.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
-const express = require('express');
+const express = require('express')
 
 const rootDir = require('../utils/path');
 
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.sendFile(path.join(rootDir,'views','admin-home.html'));
+    res.sendFile(path.join(rootDir,'views','users-home.html'));
 });
 
 module.exports = router

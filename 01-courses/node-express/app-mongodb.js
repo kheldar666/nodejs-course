adac3b82237ef4c0fd412c2a6b1467439b4da3d2
Martin Papy <martin.papy@gmail.com>
2021-08-13 12:18:48 +0700

    Small fix

--- 01-courses/node-express/app-mongodb.js
+++ 01-courses/node-express/app-mongodb.js
@@ -123,6 +123,7 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then((result) => {
     console.info("Starting Node.JS App Server");

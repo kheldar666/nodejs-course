44b3d0104149ed6a01e28001cd8c15532851681e
Martin Papy <martin.papy@gmail.com>
2021-08-23 19:48:06 +0700

    Some new things

--- 01-courses/node-es6-modules/response-handler.js
+++ 01-courses/node-es6-modules/response-handler.js
@@ -1,9 +1,24 @@
-import fs from "fs";
+import fs from "fs/promises";
 
 export const resHandler = (req, res, next) => {
-  fs.readFile("my-page.html", "utf8", (err, data) => {
-    res.send(data);
-  });
+  // fs.readFile("my-page.html", "utf8", (err, data) => {
+  //   res.send(data);
+  // });
+
+  // This won't work. ES Module don't have these global vars
+  // res.sendFile(path.join(__dirname,"my-page.html"));
+
+  // const __filename = fileURLToPath(import.meta.url);
+  // const __dirname = dirname(__filename);
+  //
+  // res.sendFile(path.join(__dirname, "my-page.html"));
+
+  // Using Promise instead of callback
+  fs.readFile("my-page.html", "utf-8")
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => console.log(err));
 };
 
 // module.exports = resHandler;
